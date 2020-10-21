90eeb9bcc1d523dbe60e311fbf3546a5b3908ca2
fujimya <aemail470@gmail.com>
2020-10-21 16:37:59 +0700

    Add files via upload

---
+++ palang_pintu/arduinonya/arduinonya.ino
@@ -0,0 +1,72 @@
+#include <Servo.h>
+
+//Ultrasonic
+int trigPin = 11; //Trig - green Jumper
+int echoPin = 10; //Echo - yellow Jumper
+int buka = 7;
+int tutup = 6;
+
+//servo
+Servo palang; // create servo object to control a servo
+int servoPinpalang = 3;
+float jarak_kereta;
+
+String statusnya = "buka";
+
+void setup() {
+  Serial.begin(115200);
+  // put your setup code here, to run once:
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  palang.attach(servoPinpalang);
+  palang.write(0);
+  pinMode(buka, OUTPUT);
+  pinMode(tutup, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  jarak_kereta = jarak();
+  Serial.print("Jaraknya : ");
+  Serial.println(jarak_kereta);
+  if (jarak_kereta < 6) {
+    if (statusnya == "buka") {
+      palang.write(0);
+      digitalWrite(buka, HIGH);
+      digitalWrite(tutup, LOW);
+      statusnya = "tutup";
+      delay(50);
+      digitalWrite(tutup, HIGH);
+    }
+
+  }
+  if (jarak_kereta >= 6) {
+    if (statusnya == "tutup") {
+      palang.write(90);
+      digitalWrite(buka, LOW);
+      digitalWrite(tutup, HIGH);
+      delay(50);
+      digitalWrite(buka, HIGH);
+      statusnya = "buka";
+    }
+
+  }
+}
+
+float jarak() {
+  // Sensor dipicu oleh pulsa HIGH dari 10us atau lebih.
+  // Berikan pulsa LOW pendek terlebih dahulu untuk memastikan pulsa HIGH bersih:
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Baca sinyal dari sensor: pulsa HIGH yang
+  // duration adalah waktu (dalam mikrodetik) dari pengirim
+  // Dari ping untuk penerimaan Echo off dari sebuah objek.
+  pinMode(echoPin, INPUT);
+  long duration = pulseIn(echoPin, HIGH);
+  // convert jarak kedalam cm dan meter
+  long cm = (duration / 2) / 29.1;
+  return cm;
+}
---
+++ palang_pintu/node_mcu/node_mcu.ino
@@ -0,0 +1,85 @@
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+
+ESP8266WiFiMulti WiFiMulti;
+
+//http
+#define pushUrl "http://service.tangkal.com/Push.php?ref=https://palangpintu.firebaseio.com/&auth=Zs4pwkm5CilcGRWUwmO569xQhmYlADeHkrnYZdg2&path=/"
+
+int pinInbuka = 16;
+int pinIntutup = 5;
+
+void setup() {
+  pinMode(pinInbuka, INPUT);
+  pinMode(pinIntutup, INPUT);
+  Serial.begin(115200);
+  int buka = digitalRead(pinInbuka);
+  int tutup = digitalRead(pinIntutup);
+  Serial.print("Buka : ");
+  Serial.println(buka);
+  Serial.print("Tutup : ");
+  Serial.println(tutup);
+  // put your setup code here, to run once:
+  WiFiMulti.addAP("samsungxiaomi", "marcello5");
+  while (WiFiMulti.run() != WL_CONNECTED) {
+    digitalWrite(2, HIGH);
+    delay(200);
+    digitalWrite(2, LOW);
+    delay(200);
+    digitalWrite(2, HIGH);
+    delay(200);
+    digitalWrite(2, LOW);
+    delay(200);
+    digitalWrite(2, HIGH);
+  }
+
+
+}
+
+//push data to firebare
+void pushData(String ref, String path, String data)
+{
+  if ((WiFiMulti.run() == WL_CONNECTED)) {
+
+    ref += path;
+    ref += "&data=";
+    ref += data;
+    HTTPClient http;
+    http.begin(ref);
+    int httpCode = http.GET();
+
+
+    if (httpCode > 0) {
+
+      if (httpCode == HTTP_CODE_OK)
+      {
+
+      }
+    }
+    else {
+    }
+    http.end();
+
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //  pushData(pushUrl, "rekam_data/palang", "tutup");
+  int buka = digitalRead(pinInbuka);
+  int tutup = digitalRead(pinIntutup);
+  Serial.print("Buka : ");
+  Serial.println(buka);
+  Serial.print("Tutup : ");
+  Serial.println(tutup);
+  if(buka == 0){
+    pushData(pushUrl, "rekam_data/palang", "buka");
+  }
+  if(tutup == 0){
+     pushData(pushUrl, "rekam_data/palang", "tutup");
+  }
+  
+  delay(50);
+
+}
